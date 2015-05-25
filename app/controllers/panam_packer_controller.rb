4a038394877dc0e381d3085ebaf7249c78b62817
Scott Kobewka <kobewka@gmail.com>
2015-05-25 19:02:24 -0400

    returning current forecast for Toronto

--- app/controllers/panam_packer_controller.rb
+++ app/controllers/panam_packer_controller.rb
@@ -5,7 +5,25 @@ class PanamPackerController < ApplicationController
 		rain       = ["Umbrella", "Rain Jacket"]
 		clear_day  = ["Sunscreen", "Hat", "Sunglasses"]
 		cool       = ["Jacket or Sweater"]
-		@forecast = ForecastIO.forecast(37.8267, -122.423)
+		
+		# Lat Long for Toronto
+		lat_long  = [43.6509802, -79.3916043]
+		forecast = get_forecast lat_long
+
+		@summary = forecast[:currently][:summary]
+		@temp    = forecast[:currently][:temperature]
+
+	end
+
+	private
+
+	# Sends a Net::HTTP request to the Forecast.io API
+	# 
+	# @param lat_long [Array] Latitude and Longitude 
+	# @param units [String] Units of measurement, defaults to SI
+	# @return [Hashie::Mash] A hash of information from Forecast.io
+	def get_forecast lat_long, units = "si"
+		ForecastIO.forecast(lat_long[0], lat_long[1], params: { units: units} )
 	end
 
 end
